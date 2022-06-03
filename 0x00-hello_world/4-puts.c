72a1596212a919a632d8080b346af9efb80a85f9
Daniel <amaize345@gmail.com>
2022-06-03 03:21:25 -0700

    Code correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
 
 /**
  *
@@ -10,12 +12,14 @@
  *
  *     */
 
+
+
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 
 }
