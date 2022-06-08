94f790e8e9b52dce6866b2aa2bb8dabeebab7661
Daniel <amaize345@gmail.com>
2022-06-07 20:27:11 -0700

    correct code

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,26 @@
 #include "main.h"
 /**
- * print_sign - Entry function
- * @n: character for sign
- * Return: Always 0.
+ * print_sign - Print the sign of a number
+ * @n: type int integer, can be negative or positive
+ * Description: print +, 0, or - depending on number, along with return
+ * Return: 1 if +, 0 if 0, and -1 if -
  */
 int print_sign(int n)
 {
-	if (n < 0)
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
-		{
-			else if (n > 0)
-			{
-				_putchar('+');
-				return (1);
-			}
-			_putchar('0');
-			return (0);
-		}
 	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
+
