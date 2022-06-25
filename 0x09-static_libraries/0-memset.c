b5efff4bc66f21b7decfcb7c8a6f5a0e8baa683e
Daniel <amaize345@gmail.com>
2022-06-24 22:03:54 -0700

    Static Library Files

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - Entry Function
+ * @s: pointer to char
+ * @b: char variable type
+ * @n: unsigned int variable type
+ * Return: Pointer to the memory areas
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+		unsigned int i;
+
+		for (i = 0; i < n; i++)
+		{
+			s[i] = b;
+		}
+		return (s);
+}
