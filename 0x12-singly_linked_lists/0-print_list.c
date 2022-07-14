eb3bc1336845d2d6457818e58406a78ada99dd8d
Daniel <amaize345@gmail.com>
2022-07-14 06:52:11 -0700

    correct code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * print_list - prints all the elements of a list_t list.
@@ -10,20 +9,20 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t l_node;
 
-	while (h)
+	l_node = 0;
+	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 
 		else
+
 			printf("[%d] %s\n", h->len, h->str);
 
-		nodes++;
 		h = h->next;
+		l_node++;
 	}
-
-	return (nodes);
+	return (l_node);
 }
-
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,28 +1,29 @@
-#ifndef _LISTS_
-#define _LISTS_
+
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stddef.h>
+
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for ALX project
+ * @str: string
+ * @len: unsign int for len
+ * @next: link to next node
  */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_s *next
+	struct list_s *next;
 } list_t;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str);
+size_t print_list(const list_t *h);
 void free_list(list_t *head);
+size_t list_len(const list_t *h);
+
+
 #endif
