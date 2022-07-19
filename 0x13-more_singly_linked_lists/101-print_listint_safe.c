e56b991b278414f50d84f5416cc6290f1f77ac41
Daniel <amaize345@gmail.com>
2022-07-18 19:35:31 -0700

    Correct code

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,13 +1,70 @@
 #include "lists.h"
 
 /**
+ * free_list - frees a linked list
+ * @head: param head list
+ */
+
+void free_list(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
  * print_listint_safe - define function
  * @head: head of a list.
  * Return: no return.
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
-	head++;
-	return (0);
+	size_t n_nodes = 0;
+	listp_t *head_ptr, *new, *add;
+
+	head_ptr = NULL;
+	while (head != NULL)
+	{
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)head;
+		new->next = head_ptr;
+		head_ptr = new;
+
+		add = head_ptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_list(&head_ptr);
+				return (n_nodes);
+			}
+		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		n_nodes++;
+	}
+
+	free_list(&head_ptr);
+	return (n_nodes);
 }
+
 
